25ec4bcf3f8f283edf7c6d277ce09adf811bed94
Faryad Azim <faryadmaalic92@yahoo.com>
2022-04-26 14:55:33 +0500

    Initializing Template

---
+++ src/Layout/Content.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <>
+    <div className="right_col h-100" role="main">
+  <div className="page-title">
+    <div className="title_left">
+      <h1>Main Heading</h1>
+    </div>
+    <div className="clearfix" />
+  </div>
+  <p>Page Contents Area...</p>
+</div>
+
+    </>
+  )
+}
+
+export default Content
---
+++ src/Layout/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+  <footer>
+  <div className="pull-right">
+    Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+  </div>
+  <div className="clearfix" />
+</footer>
+
+    </>
+  )
+}
+
+export default Footer
